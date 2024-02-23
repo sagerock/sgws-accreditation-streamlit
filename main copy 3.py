81345919b922c890b8ead812663512a1e12da7e2
sagerock <sage@sagerock.com>
2024-02-22 20:58:11 -0500

    update

--- main copy 3.py
+++ main copy 3.py
@@ -1,4 +1,6 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 import tiktoken
 import numpy as np  
 import os
@@ -11,27 +13,19 @@ import random
 from PIL import Image
 
 # When you are working locally set your api keys with this:
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-# pinecone_api_key = os.getenv('PINECONE_API_KEY')
+pinecone_api_key = os.getenv('PINECONE_API_KEY')
 
 # When you are uploading to Streamlit, set your keys like this:
-pinecone_api_key = st.secrets["API_KEYS"]["pinecone"]
-openai.api_key = st.secrets["API_KEYS"]["openai"]
+# pinecone_api_key = st.secrets["API_KEYS"]["pinecone"]
+# openai.api_key = st.secrets["API_KEYS"]["openai"]
 
 pinecone.init(api_key=pinecone_api_key, environment="us-west4-gcp")
 
-
-logo = Image.open('images/logo.jpg')
-
-
-
 # random user picture
-# user_av = random.randint(0, 100)
-user_av = 5
+user_av = 1
 
 # random bott picture
-# bott_av = random.randint(0, 100)
-bott_av = 10
+bott_av = 30
 
 def randomize_array(arr):
     sampled_arr = []
@@ -41,13 +35,10 @@ def randomize_array(arr):
         arr.remove(elem)
     return sampled_arr
 
-st.set_page_config(page_title="Spring Garden Waldorf School Accreditation AI Chatbot", layout="wide")
+st.set_page_config(page_title="Your Custom Database Driven Chatbot", page_icon="images/icon.png", layout="wide")
 
-st.header("Do you have a question about Spring Garden Waldorf School Accreditation? I might be able to help. \n")
+st.header("Ask your resources questions. \n")
 
-
-
-# st.header("Thank you for visiting the Spring Garden Waldorf School Accreditation AI Chatbot \n")
 
 # Define the name of the index and the dimensionality of the embeddings
 index_name = "sagerock"
@@ -63,9 +54,11 @@ pineconeindex = pinecone.Index(index_name)
 #######
 ######################################
 
+# Select the model you want to use.
 
 #COMPLETIONS_MODEL = "text-davinci-003"
-COMPLETIONS_MODEL = "gpt-4-0125-preview"
+COMPLETIONS_MODEL = "gpt-3.5-turbo"
+#COMPLETIONS_MODEL = "gpt-4"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 
 COMPLETIONS_API_PARAMS = {
@@ -75,23 +68,6 @@ COMPLETIONS_API_PARAMS = {
     "model": COMPLETIONS_MODEL,
 }
 
-
-main_url = "https://www.sgws.org"
-
-
-with st.sidebar:
-    st.image('images/logo.jpg', width=250)
-    st.markdown("---")
-
-    st.markdown("Welcome to the Spring Garden Waldorf School Accreditation Chatbot! \n")
-    st.markdown(
-        
-"This chatbot has the complete knowledge of the Spring Garden Waldorf School Accreditation. \n"
-
-
-"Now, go ahead and ask any question you have about the Spring Garden Waldorf School Accreditation! \n"
-
-    )
 
 
 # MAIN FUNCTIONS
@@ -108,11 +84,9 @@ def num_tokens_from_string(string, encoding_name):
 
 
 def get_embedding(text, model):
-    result = openai.Embedding.create(
-      model=model,
-      input=text
-    )
-    return result["data"][0]["embedding"]
+    result = client.embeddings.create(model=model,
+    input=text)
+    return result.data[0].embedding
 
 
 
@@ -133,7 +107,7 @@ def construct_prompt_pinecone(question):
 
     #print(xq)
 
-    res = pineconeindex.query([xq], top_k=30, include_metadata=True, namespace="cfa-site")
+    res = pineconeindex.query([xq], top_k=30, include_metadata=True, namespace="sgws-accreditation-3")
 
     #print(res)
     # print(most_relevant_document_sections[:2])
@@ -165,13 +139,11 @@ def construct_prompt_pinecone(question):
     # Useful diagnostic information
     #print(f"Selected {len(chosen_sections)} document sections:")
     
-    header = """Welcome to the Spring Garden Waldorf School Accreditation Chatbot! \n
-
-This chatbot has the complete knowledge of the Spring Garden Waldorf School Accreditation.\n"
-
-"Now, go ahead and ask any question you have about the Spring Garden Waldorf School Accreditation! \n
-
-Context: Spring Garden Waldorf School Accreditation \n
+    header = """You are a helpful AI assistant. Use the following pieces of context to answer the question at the end. \n
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer. \n
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. \n
+Also, please don't reference any charts or tables or images. People can't see them. \n
+If you don't know the answer please say that you are still new and not sure about that.  You are an expert in the Spring Garden Waldorf School Accrediation. You understand the complete document and are happy to answer any questions anyone may have about this document. \n
     """ 
     return header + "".join(chosen_sections) 
 
@@ -187,13 +159,11 @@ def summarize_past_conversation(content):
         "model": COMPLETIONS_MODEL,
     }
 
-    prompt = "Please respond to the question asked thoroughly and format it with markdown: Only responde to questions based on the content in your database of Spring Garden Waldorf School Accreditation. If you don't know the answer just let them know that you don't now and the person should reach out to the Spring Garden Waldorf School Accreditation for more information.\n" + content
+    prompt = "Summarize this discussion into a single paragraph keeping the topics mentioned: \n" + content
 
     try:
-        response = openai.Completion.create(
-                    prompt=prompt,
-                    **APPEND_COMPLETION_PARAMS
-                )
+        response = client.completions.create(prompt=prompt,
+        **APPEND_COMPLETION_PARAMS)
     except Exception as e:
         print("I'm afraid your question failed! This is the error: ")
         print(e)
@@ -210,7 +180,7 @@ def summarize_past_conversation(content):
 
 
 COMPLETIONS_API_PARAMS = {
-        "temperature": 0.7,
+        "temperature": 0.0,
         "max_tokens": 500,
         "model": COMPLETIONS_MODEL,
     }
@@ -224,14 +194,12 @@ def answer_query_with_context_pinecone(query):
     print(prompt)
     print("---------------------------------------------")
     try:
-        response = openai.ChatCompletion.create(
-                    messages=[{"role": "system", "content": "You are a highly knowledgeable chatbot that knows a great deal about Spring Garden Waldorf School Accreditation. Please only respond to questions from the information provided. If you don't know the answer just let the person know that you don't know the answer and they should reach out to Spring Garden Waldorf School for more information."},
-                            {"role": "user", "content": str(prompt)}],
-                            # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                            # {"role": "user", "content": "Where was it played?"}
-                            # ]
-                    **COMPLETIONS_API_PARAMS
-                )
+        response = client.chat.completions.create(messages=[{"role": "system", "content": "You are a helpful AI who knows a great deal about Spring Garden Waldorf School Accrediation. You understand the complete document and are happy to answer any questions anyone may have about this document. \n"},
+                {"role": "user", "content": str(prompt)}],
+                # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+                # {"role": "user", "content": "Where was it played?"}
+                # ]
+        **COMPLETIONS_API_PARAMS)
     except Exception as e:
         print("I'm afraid your question failed! This is the error: ")
         print(e)
@@ -257,7 +225,7 @@ def clear_text():
 
 # We will get the user's input by calling the get_text function
 def get_text():
-    input_text = st.text_input("Input a question here! For example: \"Tell me about the Spring Garden Waldorf School Accreditation\". \n Also, I have no memory of previous questions!😊")
+    input_text = st.text_input("Input a question here! For example: \"How do I clean lab glassware?\". \n Also, I have no memory of previous questions!😊")
     return input_text
 
 
@@ -272,11 +240,10 @@ if user_input:
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
 
-# Display chatbot responses
+
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        # Display chatbot response in Markdown format
-        st.markdown(st.session_state['generated'][i])
-        
-        # Display user input
-        message(st.session_state['past'][i], is_user=True, avatar_style="personas", seed=user_av, key=str(i) + '_user')
+        message(st.session_state["generated"][i],seed=bott_av , key=str(i))
+        message(st.session_state['past'][i], is_user=True,avatar_style="personas",seed=user_av, key=str(i) + '_user')
+
+
